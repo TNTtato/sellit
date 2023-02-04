4ef5d9603bf237957635dea51116173643f46229
Gustavo <penarandagustavo5@gmail.com>
2023-02-04 16:50:26 -0500

    test post actions

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -23,4 +23,16 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "form"
   end
+
+  test "allow to create a new product" do
+    post products_path, params: {
+      product: {
+        title: "Nintendo 64",
+        description: "Le faltan cables",
+        price: 45
+      }
+    }
+
+    assert_redirected_to products_path
+  end
 end
