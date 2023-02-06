f4bcf484aa68d590026cc09b7d1418cbab9825c2
Gustavo <penarandagustavo5@gmail.com>
2023-02-06 10:38:48 -0500

    test a successful and a not successful update operation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,7 +27,7 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
-      redirect_to products_path, notice: "Tu producto se ha actualizado"
+      redirect_to products_path, notice: "Tu producto se ha actualizado correctamente"
     else
       render :edit, status: :unprocessable_entity
     end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -56,4 +56,24 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select "form"
   end
 
+  test "allow to edit a product" do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        price: 999
+      }
+    }
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], "Tu producto se ha actualizado correctamente"
+  end
+
+  test "does not allow to edit a product" do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        price: nil
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
 end
