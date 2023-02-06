715d0955737f20294cfe68c51e3ecce3da4bb011
Gustavo <penarandagustavo5@gmail.com>
2023-02-06 14:12:00 -0500

    refactor code: resources, active record queries, clean products render

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.with_attached_photo
   end
 
   def show
-    @product = Product.find(params[:id])
+    product
   end
 
   def new
@@ -21,12 +21,11 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    product
   end
 
-  def update
-    @product = Product.find(params[:id])
-    if @product.update(product_params)
+  def update    
+    if product.update(product_params)
       redirect_to products_path, notice: "Tu producto se ha actualizado correctamente"
     else
       render :edit, status: :unprocessable_entity
@@ -34,8 +33,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
+    product.destroy
 
     redirect_to products_path, notice: "Tu producto se ha eliminado correctamente", status: :see_other
   end
@@ -44,5 +42,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :photo)
+  end
+
+  def product
+    @product = Product.find(params[:id])
   end
 end
