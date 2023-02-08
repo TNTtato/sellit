03df3e273a99f90cac001adcebe6f422d6ae6957
Gustavo <penarandagustavo5@gmail.com>
2023-02-08 13:03:36 -0500

    Filter by category using async queries and hotwire render

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Sellit
 
     # language by default
     config.i18n.default_locale = :es
+
+    # Allow multiqueries
+    config.active_record.async_query_executor = :global_thread_pool
   end
 end
